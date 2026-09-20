9a3e08443a146634a74cfd8357d5658810a36b0f
AnweshaDS <anwesha.das.bd.03@gmail.com>
2026-09-20 15:22:24 +0600

    added design tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -12,7 +11,16 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Public Sans', ...defaultTheme.fontFamily.sans],
+                display: ['Fraunces', ...defaultTheme.fontFamily.serif],
+            },
+            colors: {
+                'ink-navy': '#16233E',
+                'signal-amber': '#E4A72E',
+                'catalog-teal': '#1F6F78',
+                paper: '#F6F7F5',
+                ink: '#1A1D23',
+                slate: '#5B6472',
             },
         },
     },
